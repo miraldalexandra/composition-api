fd737b742ea79df69f7b78017987aa38aaf71d01
miralda <miraldalexandra15@gmail.com>
2021-11-19 10:59:19 +0100

    refs, reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,64 @@
 <template>
   <div class="home">
+    <h2>Home</h2>
+    <h3>refs</h3>
     <p>{{ name }} is {{ age }}</p>
-    <p ref="referenceIT">{{ name }}</p>
+    <!-- <p ref="referenceIT">{{ name }}</p> -->
     <button @click="handleClick">click</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name" />
+    <hr />
+    <h3>refs</h3>
+    <p>ninja one: {{ ninjaOne.name }} is {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">update ninja one</button>
+    <hr />
+    <h3>reactive</h3>
+    <p>ninja two: {{ ninjaTwo.name }} is {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: "Home",
-  components: {},
   setup() {
-    console.log("setup");
-    const referenceIt = ref(null);
-
-    let name = "John";
-    let age = 35;
-
+    // console.log("setup");
+    // let name = "John";
+    // let age = 35;
+    // const referenceIt = ref(null);
+    const name = ref("John");
+    const age = ref(35);
     const handleClick = () => {
-      console.log("clicked ref", referenceIt);
+      // console.log("clicked ref", referenceIt);
+      name.value = "Jack";
+      age.value = 45;
     };
+
+    // REF OBJECT
+    const ninjaOne = ref({ name: "Max", age: 38 });
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 45;
+    };
+
+    // REACTIVE
+    const ninjaTwo = reactive({ name: "reactive ninja", age: 55 });
+    const updateNinjaTwo = () => {
+      // directly access the property
+      ninjaTwo.age = 45;
+    };
+
     return {
       name,
       age,
+      // referenceIT,
       handleClick,
-      referenceIT,
+      ninjaOne,
+      updateNinjaOne,
+      ninjaTwo,
+      updateNinjaTwo,
     };
   },
   // created() {
